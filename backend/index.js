efa39f6aa623eadabe64a5c146910b41b9606212
adsa01 <adsa01@dfki.de>
2025-03-04 23:32:40 +0100

    order os match api

--- backend/index.js
+++ backend/index.js
@@ -11,10 +11,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api", matchRoutes);
+app.use("/api", facilityRoutes);
 
 connectDB();
 
-app.use("/api", facilityRoutes);
+
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
