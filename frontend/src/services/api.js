5ace636a75212db1c9f2df9808d729f382a9c2fe
adsa01 <adsa01@dfki.de>
2025-03-04 23:37:27 +0100

    fixing url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,7 +4,7 @@ const API_URL = "https://care-mvp.onrender.com"; // Replace with Render URL afte
 
 export const findFacilityMatch = async (formData) => {
     try {
-        const response = await axios.post(`${API_URL}/match`, formData);
+        const response = await axios.post(`${API_URL}/api/match`, formData);
         return response.data;
     } catch (error) {
         console.error("Error fetching match:", error);
